1c9eed69cdde74553113d07919e468832ec8cfb9
ishangrotra <92525483+ishangrotra@users.noreply.github.com>
2024-10-01 13:22:26 +0530

    Create main.py

---
+++ thug life filter/main.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+from PIL import Image
+
+maskPath = r"C:\Users\asus\Downloads\mask.png"
+
+harcasPath = r"C:\Users\asus\Downloads\harcass"
+
+faceCascade = cv2.CascadeClassifier(harcasPath)
+
+mask = Image.open(maskPath)
+
+def thug_mask(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(gray, 2.1)
+
+    background = Image.fromarray(image)
+
+    for (x, y, w, h) in faces:
+        resized_mask = mask.resize((w, h), Image.ANTIALIAS)
+
+        offset = (x, y)
+        background.paste(resized_mask, offset, mask=resized_mask)
+    
+    return np.asarray(background)
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    
+    if not ret:
+        print("Failed to capture image")
+        break
+    
+    try:
+        cv2.imshow("Thug Life Filter", thug_mask(frame))
+    except Exception as e:
+        print(f"Error applying filter: {e}")
+        break
+
+    if cv2.waitKey(1) == 27:  # Press ESC to exit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
